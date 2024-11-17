d5578543c95fd5ca24d467bdcd764171d4fd58a4
KeldiyorDev <keldiyorraxmatov1303@gmail.com>
2024-11-17 15:14:02 +0500

    index add

---
+++ studentData.js
@@ -0,0 +1,258 @@
+let talaba = [
+    {
+        "name": "NARZULLOYEV KARIMBOY",
+        "gpas": [
+            4,
+            3,
+            2.97,
+        ]
+    },
+    {
+        "name": "XOLMUHAMMADIYEVA SITORA",
+        "gpas": [
+            3.9,
+            3.2,
+            3.5,
+        ]
+    },
+    {
+        "name": "QURBONBOYEVA DIANA",
+        "gpas": [
+            4.2,
+            3.7,
+            3.5,
+        ]
+    },
+    {
+        "name": "TO‘XTAYEV FIRDAVS",
+        "gpas": [
+            3.6,
+            2.7,
+            3.9,
+        ]
+    },
+    {
+        "name": "HAMROYEVA SADOQAT",
+        "gpas": [
+            3.5,
+            3.5,
+            3.1,
+        ]
+    },
+    {
+        "name": "ELOMONOV SANJARBEK",
+        "gpas": [
+            3.4,
+            3.2,
+            3.8,
+        ]
+    },
+    {
+        "name": "O&#x27;RINOV ELDOR",
+        "gpas": [
+            3.6,
+            3,
+            3.1,
+        ]
+    },
+    {
+        "name": "RAHMATOVA DURDONA",
+        "gpas": [
+            3.9,
+            3.3,
+            3.7,
+        ]
+    },
+    {
+        "name": "QURBONOV ASADBEK",
+        "gpas": [
+            4,
+            2.6,
+            3.6,
+        ]
+    },
+    {
+        "name": "SAPAYEV OG‘ABEK",
+        "gpas": [
+            3.6,
+            2.7,
+            3.2,
+        ]
+    },
+    {
+        "name": "BOZOROV OYNAZAR",
+        "gpas": [
+            4,
+            3.9,
+            3.5,
+        ]
+    },
+    {
+        "name": "RUSTAMOV AHMADJON",
+        "gpas": [
+            2.4,
+            3.1,
+            3.5,
+        ]
+    },
+    {
+        "name": "DAVLETOV JASURBEK",
+        "gpas": [
+            4.3,
+            4.5,
+            4.7,
+        ]
+    },
+    {
+        "name": "G‘OYIBOV OG‘ABEK",
+        "gpas": [
+            3.5,
+            2.4,
+            3.1,
+        ]
+    },
+    {
+        "name": "SAYFIYEVA GULNIGOR",
+        "gpas": [
+            4.3,
+            4.2,
+            4.2,
+        ]
+    },
+    {
+        "name": "AMRILLOYEVA MOHICHEHRA",
+        "gpas": [
+            4.2,
+            4.3,
+            4.5,
+        ]
+    },
+    {
+        "name": "SHARIFJONOVA ZEBO",
+        "gpas": [
+            3.5,
+            2.9,
+            3.5,
+        ]
+    },
+    {
+        "name": "AXTAMOV BEKZOD",
+        "gpas": [
+            3.8,
+            3.7,
+            4.1,
+        ]
+    },
+    {
+        "name": "TO‘XTAYEV AZIZBEK",
+        "gpas": [
+            3.5,
+            2.1,
+            3,
+        ]
+    },
+    {
+        "name": "ABDUQODIROV MIRJALOL",
+        "gpas": [
+            3.5,
+            2.8,
+            3.1,
+        ]
+    },
+    {
+        "name": "ATABAYEV ANVAR",
+        "gpas": [
+            3.7,
+            3.2,
+            3.4,
+        ]
+    },
+    {
+        "name": "RASULOV AZIMJON",
+        "gpas": [
+            3.5,
+            2.2,
+            3.1,
+        ]
+    },
+    {
+        "name": "ODILOV MUHAMMADORIF",
+        "gpas": [
+            3.6,
+            2.1,
+            3,
+        ]
+    },
+    {
+        "name": "ASADOVA ZARNIGOR",
+        "gpas": [
+            3.8,
+            3.7,
+            4,
+        ]
+    },
+    {
+        "name": "SIROJIDDINOV MIRSHOD",
+        "gpas": [
+            3.3,
+            3.3,
+            3.7,
+        ]
+    },
+    {
+        "name": "SHARIFOVA OLTINOY",
+        "gpas": [
+            3.9,
+            3.4,
+            3.7,
+        ]
+    },
+    {
+        "name": "HOSHIMOV XAYRIDDIN",
+        "gpas": [
+            3.7,
+            1.9,
+            3,
+        ]
+    },
+    {
+        "name": "G‘AFUROVA RAYXONA",
+        "gpas": [
+            3.8,
+            3.1,
+            4.3,
+        ]
+    },
+    {
+        "name": "ABDULLAYEV AZIZBEK",
+        "gpas": [
+            3.8,
+            2.8,
+            3,
+        ]
+    },
+    {
+        "name": "G‘AFUROVA NILUFAR",
+        "gpas": [
+            3.8,
+            4,
+            4.2,
+        ]
+    },
+    {
+        "name": "RAMAZONOVA FERUZA",
+        "gpas": [
+            4.7,
+            4.8,
+            4.8,
+        ]
+    },
+    {
+        "name": "YULIYEVA IRODA",
+        "gpas": [
+            4,
+            3.6,
+            3.7,
+        ]
+    }
+]
